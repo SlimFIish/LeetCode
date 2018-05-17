80146a7d7aaf17a12abaa3b48fdc9744f55379a8
SlimFish <summeryu.seu@gmail.com>
2018-05-17 19:28:18 +0800

    [009]: Judge an integer whether is a palindrome or not

---
+++ 009_isPalindrome/isPalindrome.c
@@ -0,0 +1,37 @@
+/*
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+示例 1:
+输入: 121
+输出: true
+
+示例 2:
+输入: -121
+输出: false
+解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+
+示例 3:
+输入: 10
+输出: false
+解释: 从右向左读, 为 01 。因此它不是一个回文数
+
+*/
+bool isPalindrome(int x) {
+    int tmp, y=0, c;
+    
+    if(x < 0) return 0;
+    if(x == 0) return 1;
+    
+    tmp = x;
+    while(x){
+        c = x%10;
+        x = x/10;
+        y = 10*y + c;
+    }
+    if(y == tmp) 
+        return 1;
+    else
+        return 0;
+}
